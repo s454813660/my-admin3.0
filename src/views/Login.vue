80508b7f5ad1c89cf2379afc26ee4b8f8ec74a11
codellw <454813660@qq.com>
2020-11-26 03:09:12 +0800

    vue3..0重构

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="login">
+    <a-button type="primary">button</a-button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Login"
+};
+</script>
+<style scoped lang="scss">
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
